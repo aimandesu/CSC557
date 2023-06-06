8362dc7f82ef79cd6fe970c9a0bc9147f8f4e0c9
unknown <BingoDoom25@gmail.com>
2023-06-07 01:10:06 +0800

    place addition

--- app/src/main/java/com/example/csc557/ui/theme/place/place.kt
+++ app/src/main/java/com/example/csc557/ui/theme/place/place.kt
@@ -1,2 +1,12 @@
 package com.example.csc557.ui.theme.place
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun testing(){
+    Column(){
+        Text(text = "test")
+    }
+}
